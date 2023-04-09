c7768f88a25a9d67f7d9eeb058f66959258b6362
Kevin German <kevin.german@gmail.com>
2023-04-08 21:37:26 -0400

    dataframe

--- pgndata.py
+++ pgndata.py
@@ -1,3 +1,4 @@
+import pandas as pd
 """
 Notation:
    <number>. [Piece (default pawn)][<file>][<rank>][action {x=take}] <file><rank>[{+=check,#=mate}][evaluation tag {!,!!,!?,?!,???}] Skip to <integer>
@@ -8,23 +9,15 @@ Notation:
 
 import re
 
-def parse_moves( pgnblock="" ):
-    """
-    reads in the pgn data and generates a list of Move objects
-    """
-delim=re.compile(' \d\.')
-# move_expr='\s*(\d{1,3})\.?\s*((?:(?:O-O(?:-O)?)|(?:[KQNBR][1-8a-h]?x?[a-h]x?[1-8])|(?:[a-h]x?[a-h]?[1-8]\=?[QRNB]?))\+?)(?:\s*\d+\.?\d+?m?s)?\.?\s*((?:(?:O-O(?:-O)?)|(?:[KQNBR][1-8a-h]?x?[a-h]x?[1-8])|(?:[a-h]x?[a-h]?[1-8]\=?[QRNB]?))\+?)?(?:\s*\d+\.?\d+?m?s)?'
-move_expr='\s*(\d{1,3})'
+move_expr='\s*(?P<Move>\d{1,3})'
 white_expr='\.?\s*(((?P<W_castle>O-O(:?-O)?)|(?P<W_Piece>[KQNBR](?P<W_Origin_R_F>[1-8a-h]?)(?P<W_Takes>x?)(?P<W_File>[a-h])(?P<W_Takes1>x?)(?P<W_Rank>[1-8]))|((?P<W_PTake>[a-h]x?)(?P<W_PDestination>[a-h]?[1-8]\=?)(?P<W_PPiece>[QRNB]?)))\+?)'
 black_expr='\.?\s*(((?P<B_castle>O-O(:?-O)?)|(?P<B_Piece>[KQNBR](?P<B_Origin_R_F>[1-8a-h]?)(?P<B_Takes>x?)(?P<B_File>[a-h])(?P<B_Takes1>x?)(?P<B_Rank>[1-8]))|((?P<B_PTake>[a-h]x?)(?P<B_PDestination>[a-h]?[1-8]\=?)(?P<B_PPiece>[QRNB]?)))\+?)'
 # black_expr='(?:\s*\d+\.?\d+?m?s)?\.?\s*((?:(?:O-O(?:-O)?)|(?:[KQNBR][1-8a-h]?x?[a-h]x?[1-8])|(?:[a-h]x?[a-h]?[1-8]\=?[QRNB]?))\+?)?(?:\s*\d+\.?\d+?m?s)?'
 
 def parsemoves(pgndata):
     mover=re.compile(move_expr + white_expr + black_expr)
-    for i, move in enumerate( mover.finditer( pgndata )):
-        #if i+1 != int(move[0]):
-            #raise Exception( f"Move({i}) not equal to {move[0]} - Out of order!!!" )
-        yield move
+    data = mover.finditer(pgndata)
+    return pd.DataFrame.from_records(columns=('Move','W_castle','W_Piece','W_Origin_R_F','W_Takes','W_File','W_Takes1','W_Rank','W_PTake','W_PDestination','W_PPiece','B_castle','B_Piece','B_Origin_R_F','B_Takes','B_File','B_Takes1','B_Rank','B_PTake','B_PDestination','B_PPiece'), data=[m.groupdict() for m in mover.finditer(pgndata)], index="Move")
         
 movedata="""
 1. e4 e5 2. Nc3 Nf6 3. Nge2 d5 4. f4 exf4 5. Nxf4 dxe4 6. d3 exd3 7. Bxd3 Bd6 8.
@@ -35,5 +28,4 @@ Rb2 21. Rf4 Rb1 22. Rd4 Re8 23. Kf1 Re4 24. Rxe4 Nxe4 25. a5 Rxd1+ 26. Ke2 Rb1
 Nxa5 34. h6 g6 35. Kh2 c5 36. Kh3 c4 37. Kh2 c3 38. Kh3 c2 39. Kh2 c1=Q 40. Kh3
 Qh1# 0-1
 """
-for m in parsemoves(movedata):
-    print(m.groupdict())
+print(parsemoves(movedata))
